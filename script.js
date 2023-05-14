bda290b226da43f701c015a177e2403c06891fd1
S-Bil <86743993+S-Bil@users.noreply.github.com>
2023-05-14 13:46:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
- const username = "2CBE97003EBB";
-    const password = "vZpmxxwUw";
-    const authString = `${username}:${password}`;
-    const authHeader = "Basic " + btoa(authString);
+const username = "2CBE97003EBB";
+const password = "vZpmxxwUw";
+const authString = `${username}:${password}`;
+const authHeader = "Basic " + btoa(authString);
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", authHeader);
-    myHeaders.append("HTTP_ACCEPT", "application/json");
-    myHeaders.append("Accept", "api/v1");
-    myHeaders.append("Debug-user-agent", "Paradigma/DBG");
+const myHeaders = new Headers();
+myHeaders.append("Authorization", authHeader);
+myHeaders.append("HTTP_ACCEPT", "application/json");
+myHeaders.append("Accept", "api/v1");
+myHeaders.append("Debug-user-agent", "Paradigma/DBG");
 
-    const requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-    fetch("https://api.paradigma.remoteportal.de/controllers", requestOptions)
+fetch("https://api.paradigma.remoteportal.de/controllers", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    const controller_id = result[0]["controller_id"];
+    fetch(`https://api.paradigma.remoteportal.de/controllers/${controller_id}`, requestOptions)
       .then(response => response.json())
       .then(result => {
-        const controller_id = result[0]["controller_id"];
-        fetch("https://api.paradigma.remoteportal.de/controllers/"+controller_id, requestOptions)
-          .then(response => response.json())
-          .then(data => {
-            const daySolarEarnings = data.configuration.sensor.find(sensor => sensor.type === 'day_solarearnings');
-            document.getElementById('result').innerHTML = daySolarEarnings.value;
-          })
-          .catch(error => console.error(error));
+        // Zugriff auf last_seen
+        const lastSeen = result.last_seen;
+        console.log(`Letzter Kontakt zur Anlage: ${lastSeen}`);
+
+        // Zugriff auf die Sensordaten
+        const sensorData = result.sensor;
+        for (let i = 0; i < sensorData.length; i++) {
+          const sensorType = sensorData[i].type;
+          const sensorValue = sensorData[i].value;
+          console.log(`${sensorType} ${sensorValue}`);
+        }
       })
-      .catch(error => console.error(error));
+      .catch(error => console.log('error', error));
+  })
+  .catch(error => console.log('error', error));
